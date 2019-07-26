e47cb0145d8b7e989d1404a6f73ca2945b10aea2
TCCI_MACBOOK_PRO <tcci_macbook_pro@TCCI-MACBOOK-PROde-MacBook-Pro.local>
2019-07-26 16:13:00 +0800

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by TCCI_MACBOOK_PRO on 2019/7/24.
+//  Copyright © 2019 TCCI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+
+    var userIsInTheMiddleOfTyping: Bool = false
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathmaticalSymbol = sender.currentTitle {
+            brain.performOperation(mathmaticalSymbol)
+        }
+        if let result = brain.result {
+            displayValue = result
+        }
+    }
+    
+}
+
